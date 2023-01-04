e1345a92b4a8a2ac87be6874a6d1982fed6cbdde
GitStudentSem <ProgrammSem@yandex.ru>
2023-01-04 09:58:03 +0300

    try fix mobile height adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const StyledApp = styled.div`
     justify-content: center;
     padding: 5px;
     height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
     background: radial-gradient(
         circle,
         ${(props) => props.from} 10%,
@@ -42,7 +43,25 @@ function App() {
         "Ноябрь",
         "Декабрь",
     ];
+    const handleResize = () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
     useEffect(() => {
+        // // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+        // let vh = window.innerHeight * 0.01;
+        // // Then we set the value in the --vh custom property to the root of the document
+        // document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+        // // We listen to the resize event
+        // window.addEventListener("resize", () => {
+        //     // We execute the same script as before
+        //     let vh = window.innerHeight * 0.01;
+        //     document.documentElement.style.setProperty("--vh", `${vh}px`);
+        // });
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
         checkSizeLocalStorage();
     }, []);
     return (
